29e05d892ea9c58703348b27e51126a46238cbf0
estaesta <ahmadforesta@gmail.com>
2023-12-01 11:10:30 +0700

    test cloud run

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,25 +9,43 @@ provider "google" {
   region      = "asia-southeast2"
 }
 
-// create a new bucket
-resource "google_storage_bucket" "hijalearn-bucket" {
-  name          = "hijalearn-bucket-402105"
-  location      = "asia-southeast2"
-  storage_class = "STANDARD"
-  force_destroy = true
+# // create a new bucket
+# resource "google_storage_bucket" "hijalearn-bucket" {
+#   name          = "hijalearn-bucket-402105"
+#   location      = "asia-southeast2"
+#   storage_class = "STANDARD"
+#   force_destroy = true
+# }
+#
+# // firestore native mode
+# resource "google_project_service" "firestore" {
+#   project = var.project-id
+#   service = "firestore.googleapis.com"
+# }
+#
+# resource "google_firestore_database" "database" {
+#   name        = "hijalearn-db"
+#   project     = var.project-id
+#   location_id = "asia-southeast2"
+#   type        = "FIRESTORE_NATIVE"
+#
+#   depends_on = [google_project_service.firestore]
+# }
+
+// create cloud run service
+resource "google_cloud_run_v2_service" "default" {
+  name     = "hijalearn-service"
+  location = "asia-southeast2"
+  template {
+      containers {
+        image = "gcr.io/${var.project-id}/hijalearn-testbuild:latest"
+      }
+  }
 }
 
-// firestore native mode
-resource "google_project_service" "firestore" {
-  project = var.project-id
-  service = "firestore.googleapis.com"
-}
-
-resource "google_firestore_database" "database" {
-  name        = "hijalearn-db"
-  project     = var.project-id
-  location_id = "asia-southeast2"
-  type        = "FIRESTORE_NATIVE"
-
-  depends_on = [google_project_service.firestore]
+resource "google_cloud_run_v2_service_iam_member" "noauth" {
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
